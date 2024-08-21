d190292d76b5309ad09ef93e02746b523ce249cd
Dmitry Markov <elu.work21@gmail.com>
2024-08-22 01:09:40 +0300

    feat: :sparkles: make pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
+
   server: {
     host: "0.0.0.0",
     port: 3000,
